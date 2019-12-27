47642de64892043bc786953fd1ac15cfb9475cbb
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-12-27 14:03:35 -0600

    Add thread pool from the book.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,25 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 
+use std::sync::Mutex;
+use std::sync::Arc;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -30,4 +42,111 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+pub struct ThreadPool {
+    threads: Vec<Worker>,
+    sender: std::sync::mpsc::Sender<Message>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+
+        let (sender, receiver) = std::sync::mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            // create some threads and store them in the vector
+            threads.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool {
+            threads,
+            sender,
+        }
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &mut self.threads {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        for worker in &mut self.threads {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
+
+
+struct Worker {
+    id: usize,
+    thread: Option<std::thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<std::sync::mpsc::Receiver<Message>>>) -> Worker {
+        let thread = Some(std::thread::spawn(move || {
+            loop {
+                let message = receiver.lock().unwrap().recv().unwrap();
+
+                match message {
+                    Message::NewJob(job) => {
+                        println!("Worker {} got a job; executing.", id);
+
+                        job.call_box();
+                    },
+                    Message::Terminate => {
+                        println!("Worker {} was told to terminate.", id);
+
+                        break;
+                    },
+                }
+            }
+        }));
+
+        Worker {
+            id,
+            thread,
+        }
+    }
 }
