291b2ee78ef291b5f5dc0091beb77d43185f1db6
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-12-27 19:18:03 -0600

    Remove old code, terminate nicely.

--- src/main.rs
+++ src/main.rs
@@ -139,6 +139,12 @@ impl Thread {
     }
 }
 
+impl Drop for Thread {
+    fn drop(&mut self) {
+        self.sender.send(Message::Terminate).unwrap();
+    }
+}
+
 async fn async_main() {
     let listener = async_std::net::TcpListener::bind("127.0.0.1:7878").await.unwrap();
     let mut threads = vec![];
@@ -200,97 +206,3 @@ async fn handle_connection(mut stream: async_std::net::TcpStream) -> AsyncMsg {
 
     AsyncMsg::OldTask
 }
-
-/*pub struct ThreadPool {
-    threads: Vec<Worker>,
-    sender: std::sync::mpsc::Sender<Message>,
-}
-
-impl ThreadPool {
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-
-        let mut threads = Vec::with_capacity(size);
-
-        let (sender, receiver) = std::sync::mpsc::channel();
-
-        let receiver = Arc::new(Mutex::new(receiver));
-
-        for id in 0..size {
-            // create some threads and store them in the vector
-            threads.push(Worker::new(id, Arc::clone(&receiver)));
-        }
-
-        ThreadPool {
-            threads,
-            sender,
-        }
-    }
-
-    pub fn execute<F>(&self, f: F)
-        where
-            F: FnOnce() + Send + 'static
-    {
-        let job = Box::new(f);
-
-        self.sender.send(Message::NewJob(job)).unwrap();
-    }
-}
-
-impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        println!("Sending terminate message to all workers.");
-
-        for _ in &mut self.threads {
-            self.sender.send(Message::Terminate).unwrap();
-        }
-
-        for worker in &mut self.threads {
-            println!("Shutting down worker {}", worker.id);
-
-            if let Some(thread) = worker.thread.take() {
-                thread.join().unwrap();
-            }
-        }
-    }
-}
-
-struct Worker {
-    id: usize,
-    thread: Option<std::thread::JoinHandle<()>>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<std::sync::mpsc::Receiver<Message>>>) -> Worker {
-        let thread = Some(std::thread::spawn(move || {
-            loop {
-                let message = receiver.lock().unwrap().recv().unwrap();
-
-                match message {
-                    Message::NewJob(job) => {
-                        println!("Worker {} got a job; executing.", id);
-
-                        job.call_box();
-                    },
-                    Message::Terminate => {
-                        println!("Worker {} was told to terminate.", id);
-
-                        break;
-                    },
-                }
-            }
-        }));
-
-        Worker {
-            id,
-            thread,
-        }
-    }
-}*/
